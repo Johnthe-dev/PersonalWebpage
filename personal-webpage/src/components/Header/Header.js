7529937de8e7284d39d18ed1a20723e89f691fba
John Johnson-Rodgers <john@johnthe.dev>
2020-12-28 21:42:25 -0700

    finished temporary animation

--- personal-webpage/src/components/Header/Header.js
+++ personal-webpage/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import {Button, Form, FormControl, Image, Nav, Navbar} from "react-bootstrap";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {motion} from "framer-motion";
 
 export const Header = () => {
@@ -21,16 +21,20 @@ export const Header = () => {
         }
     };
     const viewed = sessionStorage.getItem('viewed');
-    if (viewed!==true){
-        svgVariance = {
-            hidden:{
-
-            },
-            visible:{
-
-            }
-        }
-    }
+    console.log(viewed);
+    let rectangles = (number)=>{
+            return {
+                hidden:{
+                    opacity: !viewed?0:1
+                },
+                visible:{
+                    opacity: 1,
+                    transition: {
+                        delay: number/4
+                    }
+                }
+            };
+    };
     const scaleFactor=2;
     const PageActive = useState({
         'Home': false,
@@ -44,45 +48,45 @@ export const Header = () => {
     const logo = <motion.svg xmlns="http://www.w3.org/2000/svg" width={scaleSvg(100)} height={scaleSvg(37)} variants={svgVariance}
                              initial={'hidden'} animate={'visible'}>
         <motion.g id="Logo_Image" transform="" variants={logoIconRoll}>
-            <g id="Rectangle_1" className="rectangles" transform={"translate("+scaleSvg(9.21)+")"} stroke="#252422">
+            <motion.g id="Rectangle_1" className="rectangles" transform={"translate("+scaleSvg(9.21)+")"} stroke="#252422" variants={rectangles(1)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_2" className="rectangles" transform={"translate("+scaleSvg(8.95)+" "+scaleSvg(.15)+") rotate(45)"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_2" className="rectangles" transform={"translate("+scaleSvg(8.95)+" "+scaleSvg(.15)+") rotate(45)"}
+               stroke="#252422" variants={rectangles(2)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_3" className="rectangles" transform={"translate("+scaleSvg(22.0)+" "+scaleSvg(.15)+") rotate(45)"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_3" className="rectangles" transform={"translate("+scaleSvg(22.0)+" "+scaleSvg(.15)+") rotate(45)"}
+               stroke="#252422" variants={rectangles(8)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_4" className="rectangles" transform={"translate("+scaleSvg(18.45)+" "+scaleSvg(9.21)+")"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_4" className="rectangles" transform={"translate("+scaleSvg(18.45)+" "+scaleSvg(9.21)+")"}
+               stroke="#252422" variants={rectangles(7)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_5" className="rectangles" transform={"translate("+scaleSvg(9.21)+" "+scaleSvg(18.45)+")"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_5" className="rectangles" transform={"translate("+scaleSvg(9.21)+" "+scaleSvg(18.45)+")"}
+               stroke="#252422" variants={rectangles(5)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_6" className="rectangles" transform={"translate("+scaleSvg(22.0)+" "+scaleSvg(13.1)+")"+" rotate(45)"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_6" className="rectangles" transform={"translate("+scaleSvg(22.0)+" "+scaleSvg(13.1)+")"+" rotate(45)"}
+               stroke="#252422" variants={rectangles(6)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_7" className="rectangles" transform={"translate("+scaleSvg(8.95)+" "+scaleSvg(13.1)+")"+" rotate(45)"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_7" className="rectangles" transform={"translate("+scaleSvg(8.95)+" "+scaleSvg(13.1)+")"+" rotate(45)"}
+               stroke="#252422" variants={rectangles(4)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
-            <g id="Rectangle_8" className="rectangles" transform={"translate("+scaleSvg(0)+" "+scaleSvg(9.2075)+")"}
-               stroke="#252422">
+            </motion.g>
+            <motion.g id="Rectangle_8" className="rectangles" transform={"translate("+scaleSvg(0)+" "+scaleSvg(9.2075)+")"}
+               stroke="#252422" variants={rectangles(3)}>
                 <rect width={scaleSvg(12.5)} height={scaleSvg(12.5)}/>
                 <rect x={scaleSvg(-.25)} y={scaleSvg(-.25)} width={scaleSvg(13.0)} height={scaleSvg(13.0)}/>
-            </g>
+            </motion.g>
         </motion.g>
         <g id="Logo_Words" transform={"translate("+scaleSvg(34.9)+")"}>
             <text id="John" className="text" transform={"translate("+scaleSvg(15.6)+" "+scaleSvg(6)+")"} fill="#252422">
@@ -106,6 +110,8 @@ export const Header = () => {
         default:
             break;
     }
+    !viewed&&sessionStorage.setItem('viewed', 'true');
+
     return (
         <Navbar bg="light" expand="lg" className={'border-info border-bottom'}>
             <Navbar.Brand href="#home">{logo}</Navbar.Brand>
