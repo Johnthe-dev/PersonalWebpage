ddab3376e15711fdb5399a5981051ba311d56e8f
John Johnson-Rodgers <john@johnthe.dev>
2020-12-31 17:43:04 -0700

    decided to go with php backend, may convert to node after some research.

---
+++ sql/ddlScript.sql
@@ -0,0 +1,22 @@
+use blog;
+
+drop table if exists relationships;
+drop table if exists post;
+
+-- this table will contain the posts
+create table post (
+    postId VARCHAR(250) not null, -- specially formatted abcde-12345-edced would be post edced in category abcde and subcategory 12345. May revisit later.
+    postTitle VARCHAR(200) not null,
+    postDate DATETIME(6) not null,
+    postContent TEXT,
+    primary key (postId)
+);
+
+-- this table will contain related posts as a weak entity
+create table relationships (
+    firstPost varchar(250) not null,
+    secondPost varchar(250) not null,
+    foreign key (firstPost) references post(postId),
+    foreign key (secondPost) references post(postId),
+    primary key (firstPost, secondPost)
+);
