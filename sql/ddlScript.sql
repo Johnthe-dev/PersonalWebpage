9f03c882f5cd34b2242726d1e4129fdac93ea0c7
John Johnson-Rodgers <john@johnthe.dev>
2021-02-04 20:53:03 -0700

    finished the Post Class api

--- sql/ddlScript.sql
+++ sql/ddlScript.sql
@@ -6,9 +6,9 @@ drop table if exists post;
 -- this table will contain the posts
 create table post (
     postId VARCHAR(250) not null, -- specially formatted abcde-12345-edced would be post edced in category abcde and subcategory 12345. May revisit later.
-    postTitle VARCHAR(200) not null,
-    postDate DATETIME(6) not null,
     postContent TEXT,
+    postDate DATETIME(6) not null,
+    postTitle VARCHAR(200) not null,
     primary key (postId)
 );
 
